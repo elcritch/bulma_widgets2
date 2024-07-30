9498df70417268513200ca59956abc8c530e3d1c
Jaremy Creechley <creechley@gmail.com>
2024-07-30 00:58:29 -0400

    first commit

---
+++ lib/bulma_components.ex
@@ -0,0 +1,18 @@
+defmodule BulmaComponents do
+  @moduledoc """
+  Documentation for `BulmaComponents`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BulmaComponents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bulma_components_test.exs
@@ -0,0 +1,8 @@
+defmodule BulmaComponentsTest do
+  use ExUnit.Case
+  doctest BulmaComponents
+
+  test "greets the world" do
+    assert BulmaComponents.hello() == :world
+  end
+end

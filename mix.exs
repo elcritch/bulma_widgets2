3b35c5cbd2814d0720c99c16836c7c6f0c03409f
Jaremy Creechley <creechley@gmail.com>
2024-08-03 03:15:58 -0400

    bump version - cleanups

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BulmaWidgets.MixProject do
   def project do
     [
       app: :bulma_widgets,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

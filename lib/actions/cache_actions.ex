705b28675f5ed6bc2a1189be2746eb10e8eca9fe
Jaremy Creechley <creechley@gmail.com>
2024-08-03 04:14:05 -0400

    tweaks

--- lib/actions/cache_actions.ex
+++ lib/actions/cache_actions.ex
@@ -20,9 +20,9 @@ defmodule BulmaWidgets.Action.CacheUpdate do
   alias BulmaWidgets.Event
   require Logger
 
-  def call(%Event{data: {key, values}, socket: socket} = evt, opts \\ []) do
+  def call(%Event{data: {_key, values}, socket: socket} = evt, opts \\ []) do
     topic = opts |> Keyword.get(:topic, [])
-    values = opts |> Keyword.get(:values, %{key => values}) |> Event.fields_to_assigns()
+    values = opts |> Keyword.get(:values, values) |> Event.fields_to_assigns()
     view = BulmaWidgets.Action.CacheState # use single global cache for now to match broadcast
 
     BulmaWidgets.Cache.put_all(view, topic, values)

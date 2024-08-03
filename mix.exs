9e8561ac44cf66afe1701c477636e6223e07e880
Jaremy Creechley <creechley@gmail.com>
2024-08-03 18:47:03 -0400

    configs

--- mix.exs
+++ mix.exs
@@ -6,55 +6,67 @@ defmodule BulmaWidgets.MixProject do
       app: :bulma_widgets,
       version: "0.4.1",
       elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  # Configuration for the OTP application.
+  #
   def application do
     [
-      extra_applications: [:logger]
+      mod: {BulmaWidgets.Application, []},
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
   defp deps do
     [
       {:phoenix, "~> 1.7.14"},
       {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 1.0.0-rc.1", only: [:test, :dev]},
+
       {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      # {:swoosh, "~> 1.5"},
-      # {:finch, "~> 0.13"},
-      # {:telemetry_metrics, "~> 1.0", override: true},
-      # {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
+      {:telemetry_metrics, "~> 1.0", only: [:test, :dev]},
+      {:telemetry_poller, "~> 1.0", only: [:test, :dev]},
       {:jason, "~> 1.2"},
-      # {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"},
+      {:dns_cluster, "~> 0.1.1", only: [:test, :dev]},
+      {:bandit, "~> 1.5", only: [:test, :dev]}
 
       {:dart_sass, "~> 0.6", only: [:test, :dev]},
 
       {:ex_cldr_dates_times, "~> 2.4", optional: true},
       {:ex_cldr, "~> 2.16", optional: true},
-
-
     ]
   end
 
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "assets.setup", "assets.deploy"],
+      # setup: ["deps.get", "assets.setup", "assets.deploy"],
+      setup: ["deps.get", "assets.setup", "assets.build"],
       "assets.setup": ["esbuild.install --if-missing"],
+      "assets.build": ["esbuild bulma_widgets"],
       "assets.deploy": [
-        # "esbuild bulma_widgets --minify",
+        "esbuild bulma_widgets --minify",
         "sass default --no-source-map --style=compressed",
         "phx.digest"
       ]
     ]
   end
-
 end

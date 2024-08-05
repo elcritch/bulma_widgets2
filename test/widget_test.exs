ab980972ee31656eef435023faf54670166cd155
Jaremy Creechley <creechley@gmail.com>
2024-08-05 16:32:55 -0400

    updates

--- test/widget_test.exs
+++ test/widget_test.exs
@@ -38,6 +38,36 @@ defmodule BulmaWidgetsTest.Widgets do
 
   end
 
+  test "test broadcast", %{conn: conn} do
+    # set cache value
+    BulmaWidgets.Cache.clear!()
+
+    {:ok, view, html} = live(conn, "/widgets.html")
+    # Load another view and verify it's updated
+    {:ok, view2, html2} = live(conn, "/widgets.html")
+
+    # view |> prettyElement("wiper_options", "#wiper_options " )
+    refute has_element?(view, @first_item <> " > a[class*=is-active]")
+    refute has_element?(view, @second_item <> " > a[class*=is-active]")
+    refute has_element?(view2, @first_item <> " > a[class*=is-active]")
+    refute has_element?(view2, @second_item <> " > a[class*=is-active]")
+
+    # select menu on first view, should broadcast update to second view
+    res =
+      view
+        |> element(@first_item <> " > a:nth-child(1)")
+        |> render_click()
+
+    view |> prettyElement("wiper_options", "#wiper_options " )
+    assert has_element?(view, @first_item <> " > a[class*=is-active]")
+    refute has_element?(view, @second_item <> " > a[class*=is-active]")
+
+    view2 |> prettyElement("wiper_options", "#wiper_options " )
+    assert has_element?(view2, @first_item <> " > a[class*=is-active]")
+    refute has_element?(view2, @second_item <> " > a[class*=is-active]")
+
+  end
+
   test "manually set cache item", %{conn: conn} do
     # set cache value
     BulmaWidgets.Cache.clear!()
@@ -45,7 +75,6 @@ defmodule BulmaWidgetsTest.Widgets do
     data = %{wiper_options: {"Inverted", -1}}
     BulmaWidgets.Cache.put(BulmaWidgets.Action.CacheState, key, data)
 
-    # {:ok, view, html} = live(conn, "/examples/selection_menu")
     {:ok, view, html} = live(conn, "/widgets.html")
 
     # view |> prettyElement("element 2", "#wiper_options " )
@@ -58,7 +87,6 @@ defmodule BulmaWidgetsTest.Widgets do
     # set cache value
     BulmaWidgets.Cache.clear!()
 
-    # {:ok, view, html} = live(conn, "/examples/selection_menu")
     {:ok, view, html} = live(conn, "/widgets.html")
 
     # view |> prettyElement("wiper_options", "#wiper_options " )

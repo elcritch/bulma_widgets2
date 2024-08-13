2040ce0c31eafa731201ce87b8c52cfcde955928
Jaremy Creechley <creechley@gmail.com>
2024-08-13 13:40:36 -0600

    add tab

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BulmaWidgets.MixProject do
   def project do
     [
       app: :bulma_widgets,
-      version: "0.5.4",
+      version: "0.5.5",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

93eb847965f6d37594e3021a1dc9a0238142e469
Jaremy Creechley <creechley@gmail.com>
2024-09-19 23:28:48 -0600

    rpc

--- lib/components/elements.ex
+++ lib/components/elements.ex
@@ -217,8 +217,6 @@ defmodule BulmaWidgets.Elements do
   slot(:inner_block, required: true)
 
   def button(assigns) do
-    Logger.info("BUTTON: #{inspect(assigns.rest, pretty: true)}")
-
     ~H"""
     <button class={["button", classes(@rest)]} {extras(@rest)}>
       <%= render_slot(@inner_block) %>

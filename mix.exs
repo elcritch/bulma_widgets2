d8de5bc1a5b8eda09f015bbb0dc981a4894063de
Jaremy Creechley <creechley@gmail.com>
2024-11-07 21:17:44 -0700

    seperate web example for only dev env

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule BulmaWidgets.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "lib/bulma_widgets_web", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "examples/bulma_widgets_web", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
